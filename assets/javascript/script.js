ff1e30cb0dfdaf1383165843a9e489e07ec705aa
Gina Paik <ginapaik@outlook>
2018-07-02 16:39:19 -0400

    made a photo from the JSON object appear on the page

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,11 +1,10 @@
 
-$("button").on("click", function() {
-
-       // MapQuest API
-       // var queryURL = "https://www.mapquestapi.com/geocoding/v1/address?key=xG2AsaO7IQyvtx95sqKCAAX2KFf3jyzS&location=1600+Pennsylvania+Ave+NW,Washington,DC,20500";
-
-       var queryURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=pho&key=xG2AsaO7IQyvtx95sqKCAAX2KFf3jyzS";
-     // Performing an AJAX request with the queryURL
+$(".nearYou").on("click", function() {
+    
+    // var latlon = 38.9015023 + "," + -77.0505726;
+    // //    var queryURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location" + latlon+ "&radius=1500&type=restaurant&keyword=pho&key=AIzaSyB3S0uLo0IY9-hlsfrM3aa-jDCLm9ffE6M";
+    // var queryURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location" + latlon + "&keyword=pho&key=AIzaSyB3S0uLo0IY9-hlsfrM3aa-jDCLm9ffE6M"
+    var queryURL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=pho&inputtype=textquery&fields=photos,formatted_address,name,opening_hours,rating&locationbias=circle:2000@47.6918452,-122.2226413&key=AIzaSyB3S0uLo0IY9-hlsfrM3aa-jDCLm9ffE6M";
      $.ajax({
        url: queryURL,
        method: "GET"
@@ -14,54 +13,60 @@ $("button").on("click", function() {
        .then(function(response) {
 
          console.log(response);
+           
+         $("#pho").text(response.candidates[0].name);
 
+         var imgURL = response.candidates["0"].photos["0"].photo_reference;
+
+          // Creating an element to hold the image
+          var image = $("<img>").attr("src", "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=" + imgURL + "&key=AIzaSyB3S0uLo0IY9-hlsfrM3aa-jDCLm9ffE6M");
+          var mapDiv = $("#map");
+          mapDiv.append(image);
        });
    });
 
 
 
-var x = document.getElementById("demo");
+// var x = document.getElementById("demo");
 
-function getLocation() {
-   if (navigator.geolocation) {
-       navigator.geolocation.getCurrentPosition(showPosition, showError);
-   } else {
-       x.innerHTML = "Geolocation is not supported by this browser.";
-   }
-}
+// function getLocation() {
+//    if (navigator.geolocation) {
+//        navigator.geolocation.getCurrentPosition(showPosition, showError);
+//    } else {
+//        x.innerHTML = "Geolocation is not supported by this browser.";
+//    }
+// }
 
-function showPosition(position) {
-   var latlon = position.coords.latitude + "," + position.coords.longitude;
-   var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="
-   +latlon+"&zoom=14&size=400x300&key=AIzaSyBu-916DdpKAjTmJNIgngS6HL_kDIKU0aU";
-   document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
-}
-//To use this code on your website, get a free API key from Google.
-//Read more at: https://www.w3schools.com/graphics/google_maps_basic.asp
+// function showPosition(position) {
+//    var latlon = position.coords.latitude + "," + position.coords.longitude;
+//    var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="
+//    +latlon+"&zoom=14&size=400x300&key=AIzaSyBu-916DdpKAjTmJNIgngS6HL_kDIKU0aU";
+//    document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
+// }
 
-function showError(error) {
-   switch(error.code) {
-       case error.PERMISSION_DENIED:
-           x.innerHTML = "User denied the request for Geolocation."
-           break;
-       case error.POSITION_UNAVAILABLE:
-           x.innerHTML = "Location information is unavailable."
-           break;
-       case error.TIMEOUT:
-           x.innerHTML = "The request to get user location timed out."
-           break;
-       case error.UNKNOWN_ERROR:
-           x.innerHTML = "An unknown error occurred."
-           break;
-   }
-}
+// function showError(error) {
+//    switch(error.code) {
+//        case error.PERMISSION_DENIED:
+//            x.innerHTML = "User denied the request for Geolocation."
+//            break;
+//        case error.POSITION_UNAVAILABLE:
+//            x.innerHTML = "Location information is unavailable."
+//            break;
+//        case error.TIMEOUT:
+//            x.innerHTML = "The request to get user location timed out."
+//            break;
+//        case error.UNKNOWN_ERROR:
+//            x.innerHTML = "An unknown error occurred."
+//            break;
+//    }
+// }
 
 
 
-$("#near-you").on("click", function(event) {
-   event.preventDefault();
-});
+// $("#near-you").on("click", function(event) {
+//    event.preventDefault();
+// });
 
-$("#Zipcode").on("click", function(event) {
-    event.preventDefault();
-  });
+// $("#Zipcode").on("click", function(event) {
+//     event.preventDefault();
+//   });
